249f3eb4885d3a3e7c9cabd7685ea2bc0f5658fe
DESKTOP-8NADJPO\yb010 <yb010626@gmail.com>
2021-08-12 15:53:54 +0900

    [김유빈] -ViewResolver로 jsp view 화면 띄우기 연습문제(최종)

--- src/main/java/com/starkyb/spring/lesson01/Test03.java
+++ src/main/java/com/starkyb/spring/lesson01/Test03.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class Test03 {
 	@RequestMapping("/lesson01/test03/1")
 	public String view() {
-		return "/lesson01/test03";
+		return "lesson01/test03";
 	}
 }
