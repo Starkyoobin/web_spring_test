3acf916c0caf43a001ae3cb51e61f38f2b74fa1f
DESKTOP-8NADJPO\yb010 <yb010626@gmail.com>
2021-08-23 14:08:58 +0900

    [김유빈] -JSTL Core 라이브러리 1 연습문제(피드백 필요)

---
+++ src/main/java/com/starkyb/spring/lesson05/Lesson05Controller.java
@@ -0,0 +1,14 @@
+package com.starkyb.spring.lesson05;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/lesson05")
+public class Lesson05Controller {
+	@GetMapping("/test01")
+	public String test01() {
+		return "lesson05/test01";
+	}
+}
